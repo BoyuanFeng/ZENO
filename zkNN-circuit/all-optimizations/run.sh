2e3c373d8466b020997914b2d017950748fe27dd
EC2 Default User <ec2-user@ip-172-31-31-105.ap-northeast-1.compute.internal>
2023-10-31 17:55:14 +0000

    update

--- zkNN-circuit/all-optimizations/run.sh
+++ zkNN-circuit/all-optimizations/run.sh
@@ -1,7 +1,7 @@
 rustup override set 1.43.0
 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin shallownet_knit_encoding --release > result/shallownet_knit.log
-CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin lenet_small_cifar_knit_encoding --release > result/small_cifar_knit.log
-CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin lenet_medium_cifar_knit_encoding --release > result/medium_cifar_knit.log
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin lenet_cifar_small_knit_encoding --release > result/lenet_cifar_small.log
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin lenet_cifar_large_knit_encoding --release > result/lenet_cifar_large.log
 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin vgg_one_private --release > result/vgg_one_private.log
 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin resnet18_one_private --release > result/resnet18_one_private.log
 CARGO_NET_GIT_FETCH_WITH_CLI=true cargo --frozen run --bin vgg_both_private --release > result/vgg_both_private.log
